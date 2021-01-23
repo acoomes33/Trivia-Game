f9ae48966ca63312778c9474e5f6a3357a5042a2
Alan Coomes <acoomes33@gmail.com>
2021-01-22 22:15:26 -0600

    skeleton Home component structure

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Trivia! Now let's see how much you really know...</h1>
+    </div>
+  );
+}
+
+export default Home;
